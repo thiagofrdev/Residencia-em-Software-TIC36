165a67561c715c62199ff4a9a92318afd31c183f
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2024-09-16 15:43:56 -0300

    Adicionada pasta 'TypeScript'

---
+++ UNIDADE 3 - JavaScript/TypeScript/dist/js/index.js
@@ -0,0 +1,14 @@
+"use strict";
+let x = 10; //Típico do JS
+let y = 20; //COm o TS é possível adicionar tipos
+console.log(x + " " + y);
+//x = 'teste1' -> ERRO
+//y = 'teste2' -> ERRO
+y = 2;
+console.log(x + " " + y);
+//tipos básicos
+let fristName = "Thiago";
+let agr = 20;
+const isAdmin = true;
+//Objeto
+const numbers = [1, 2, 3];
---
+++ UNIDADE 3 - JavaScript/TypeScript/src/index.ts
@@ -0,0 +1,16 @@
+let x = 10 //Típico do JS
+let y:number = 20 //COm o TS é possível adicionar tipos
+console.log(x+" "+y)
+
+//x = 'teste1' -> ERRO
+//y = 'teste2' -> ERRO
+y = 2
+console.log(x+" "+y) 
+
+//tipos básicos
+let fristName: string = "Thiago"
+let agr:number = 20
+const isAdmin:boolean = true
+
+//Objeto
+const numbers:number[] = [1,2,3]
